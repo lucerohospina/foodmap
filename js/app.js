7feb58fed7c1a1e6790329a699cf569c9648ca43
lucerohospina <lucerojhp@gmail.com>
2017-12-24 17:51:45 -0500

    agregando funcionalidad a filtro seleccionador de restaurantes

--- js/app.js
+++ js/app.js
@@ -1,10 +1,21 @@
 $(document).ready(function() {
+  /* Animación splash de intro */
   $(function() {
     setTimeout(function() {
       $('#intro').fadeOut(1000);
     }, 2000);
   });
-  
+
+  /* Dando funcionalidad al filtro seleccionador */
+
+  var $select = $('#rest-filter');
+
+  $select.on('change', restFilter);
+
+  function restFilter() {
+    var $kindOfFood = $select.val();
+    console.log($kindOfFood);
+  }
 });
 
 /* window.location.href = 'ruta donde esta mi archivo html */
