2ec1e38037e18b007467f91a8344a1a08ce40921
lucerohospina <lucerojhp@gmail.com>
2017-12-30 13:31:04 -0500

    agregando modal a las imagenes

--- js/app.js
+++ js/app.js
@@ -6,47 +6,43 @@ $(document).ready(function() {
     }, 2000);
   });
 
-  /* Dando funcionalidad al filtro seleccionador */
+  /* Definiendo variables */
   var $imgContainer = $('#img-container-1');
   var $imgContainer2 = $('#image-container-2');
   var $imgContainer3 = $('#image-container-3');
   var $imgContainer4 = $('#image-container-4');
   var $select = $('#rest-filter');
-  var $picture = $('.pic');
-  
+  var $picture = $('img');
+
+  /* Evento con el mouse encima de cada imagen */
   $picture.on('mouseover', addEffect);
+
+  /* Dando funcionalidad al filtro seleccionador */
   $select.on('change', selectRest);
-  
-  
+
+  /* Funciones*/
   function addEffect() {
     $picture.addClass('effect');
   }
-
+  
   function selectRest() {
     console.log($select.val());
     $imgContainer.hide();
     for (i = 0; i < data.length; i++) {
       if ($select.val() === 'criolla' && data[i]['type'] === 'criollo') {
-        $imgContainer2.append('<img class="col-xs-4 pic" src="' + data[i].image + '">');
-        
+        $imgContainer2.append('<img class="col-xs-4 pic" data-toggle="modal" data-target="#rest-modal" src="' + data[i].image + '">');
         console.log(data[i]['image']);
       } else if ($select.val() === 'marina' && data[i]['type'] === 'marino') {
         $imgContainer2.hide();
-        
-        $imgContainer3.append('<img class="col-xs-4 pic" src="' + data[i].image + '">');
-        
+        $imgContainer3.append('<img class="col-xs-4 pic" data-toggle="modal" data-target="#rest-modal" src="' + data[i].image + '">');
         console.log(data[i]['image']);
       } else if ($select.val() === 'pizzeria' && data[i]['type'] === 'pizza') {
         $imgContainer3.hide();
-        $imgContainer4.append('<img class="col-xs-4 pic" src="' + data[i].image + '">');
-        
+        $imgContainer4.append('<img class="col-xs-4 pic" data-toggle="modal" data-target="#rest-modal" src="' + data[i].image + '">');
         console.log(data[i]['image']);
       }
-     
     }
-    
-  } 
-  
+  }
 });
 
 /* Agregando el gmap */
@@ -63,5 +59,3 @@ function initMap() {
   });
 }
 /* Fin de mapa */
-
-/* window.location.href = 'ruta donde esta mi archivo html' */
